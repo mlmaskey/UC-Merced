5e96e52e00786930b85832665e6104426289b385
Mahesh Lal Maskey <45472541+mlmaskey@users.noreply.github.com>
2020-02-10 14:26:13 -0800

    Add files via upload

---
+++ CALVIN-Demo/Example/main.py
@@ -0,0 +1,14 @@
+from calvin import *
+from postprocessor import *
+
+# specify data file
+calvin = CALVIN('links_infeasible.csv')
+
+# create pyomo model from specified data file
+calvin.create_pyomo_model(debug_mode=True)
+
+# solve the problem
+calvin.solve_pyomo_model(solver='glpk', nproc=1, debug_mode=True)
+
+# postprocess results to create time-series files
+postprocess(calvin.df, calvin.model, resultdir='results4')
